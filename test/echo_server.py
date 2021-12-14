14bff05350323fd977c1eea9cde3fb0f7c9da0da
s-ball <s-ball@laposte.net>
2021-12-14 08:29:52 +0100

    Implement an echoing server for the tests.

---
+++ test/echo_server.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+
+class EchoRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        data = b'Request line: ' + self.requestline.encode() + b'\r\n'
+        data += b'Headers:\r\n' + self.headers.as_bytes()
+        length = int(self.headers.get('content-length', '0'))
+        if length > 0:
+            data += b'\r\nData:\r\n'
+            data += self.rfile.read(length)
+        self.send_response(200)
+        self.send_header('Content-Length', str(length))
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(data)
+
+    def do_POST(self):
+        self.do_GET()
+
+
+class EchoServer(HTTPServer, threading.Thread):
+    def __init__(self, host, port):
+        HTTPServer.__init__(self, (host, port), EchoRequestHandler)
+        threading.Thread.__init__(self)
+
+    def run(self) -> None:
+        self.serve_forever()
